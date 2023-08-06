055559406dfcc78d99a99667fa1e8eeaeab99e1a
Imane Belaid <belaidimane@gmail.com>
2023-08-06 17:36:25 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+//setBestCustomer()
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+//    overwriteBestCustomer()
+
+const leastFavoriteCustomer = "Sophia";
+function changeLeastFavoriteCustomer() { 
+    leastFavoriteCustomer = "Eman"; 
+}
+
